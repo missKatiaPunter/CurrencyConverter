3e66371054d567279fa7748e65abfc0f40b8ad6d
Katia <katia.punter@storyshareplatform.com>
2020-12-05 16:33:39 +0000

    Project structure

--- App/index.js
+++ App/index.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>LalalOpen up App.js to start working on your app!</Text>
+      <Text>Lalalala</Text>
       <StatusBar style="auto" />
     </View>
   );
