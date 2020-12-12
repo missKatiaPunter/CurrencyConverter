e392e57aa9b4330ea17fe5ef2507c03650fcfc6a
Katia <katia.punter@storyshareplatform.com>
2020-12-12 11:35:44 +0000

    Adds a button to ConversionInput

---
+++ App/components/ConversionInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const ConversionInput = () => {
+  return(
+    <View>
+      <TouchableOpacity>
+        <Text> Input </Text>
+      </TouchableOpacity>
+      
+    </View>
+  )
+}
+
+export default ConversionInput;
